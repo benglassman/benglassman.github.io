7fa11dd5e470d1e3f6b8e89b1f6a7dd1580ab26b
benglassman <bhglassman@gmail.com>
2015-02-10 15:57:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ var DoD = [
     new Date(2012,0,30),
     new Date(2012,4,3),
     new Date(2013,7,6),
-    ,,
+    ,
     new Date(2013,4,3),,
-    new Date(2011,4,28)
+    new Date(2011,4,28),,
     ];
 var DoFP = [,
     new Date(2011,10,11), , , ,new Date(2013,0,28), ,,,]
@@ -21,17 +21,51 @@ var PDD = [
     new Date(2013,11,24)
 ];
 var eventTime=[];
-var indicator = [];
+var indicator = []
 var currentDate= new Date();
 
 for(var i=0; i<PDD.length; i++){
     if(DoFP[i]){
         eventTime[i]= (DoFP[i].getTime()-PDD[i])/(1000 * 3600 * 24);
+        indicator[i] = 0;
     } else if(DoD[i]){
         eventTime[i]= (DoD[i].getTime()-PDD[i].getTime())/(1000 * 3600 * 24);
+        indicator[i]=0;
     } else{
-        eventTime[i]= (currentDate.getTime()-PDD[i].getTime())/(1000 * 3600 * 24);
+        eventTime[i]= (currentDate.getTime()-PDD[i].getTime())/(1000 * 3600 * 24)
+        indicator[i]=1;
+}
+    }
+
+var x = 0;
+var len = eventTime.length;
+while(x < len){ 
+    eventTime[x] = eventTime[x].toFixed(0); 
+    x++
 }
 
+document.getElementById("demo").innerHTML = eventTime
 
-document.getElementById("demo").innerHTML = (currentDate.getTime()-PDD[1].getTime())/(1000 * 3600 * 24);
+var atrisk = eventTime.length;
+var cen = 0;
+var calc = 1;
+debugger
+var time = [];
+var result = [];
+
+for(var i = 0; i< 35; i=i++){
+    for(var j = 0; j< eventTime.length, j++){
+        cen = 0
+        var deaths =0;
+        if(eventTime[j] < i*30){
+            cen = cen + indicator[j]
+            if(indicator[j] == 0){
+                deaths++
+            }
+        }
+        time[i]=i*30
+        calc =calc * (atrisk-deaths)/atrisk
+        result[i] = calc
+    }
+    
+}
